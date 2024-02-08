36b7f644197f020d8ba90648930430a8376692c7
devinschumacher <devin@serp.co>
2024-02-08 10:40:04 +0900

    retry

--- src/utils/cloudflare.js
+++ src/utils/cloudflare.js
@@ -1,5 +1,17 @@
 // utils/cloudflare.js
 
 export function getCloudflareResizedImageURL(url, width, height) {
-  return url.replace(/w=\d+/, `w=${width}`).replace(/h=\d+/, `h=${height}`);
+  // Split the URL into parts
+  let parts = url.split('/');
+
+  // Find the part of the URL with the width and height
+  let sizePartIndex = parts.findIndex((part) => part.startsWith('w='));
+
+  if (sizePartIndex !== -1) {
+    // Replace the width and height in the URL part
+    parts[sizePartIndex] = `w=${width},h=${height},fit=pad,background=black`;
+  }
+
+  // Join the parts back together into a URL
+  return parts.join('/');
 }
