d349da3baa9dc89e778f25ec5c121517f1991765
devinschumacher <devin@serp.co>
2024-02-22 23:22:14 -0800

    update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,14 +5,22 @@ import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
-  const feedItems = posts.map((post) => ({
+  // Fetch posts from multiple collections
+  const blogPosts = await getCollection('blog');
+  const gunLawsPosts = await getCollection('gun-laws');
+  const huntingLawsPosts = await getCollection('hunting-laws');
+  const shopPosts = await getCollection('shop');
+
+  // Combine posts from all collections
+  const allPosts = [...blogPosts, ...gunLawsPosts, ...huntingLawsPosts, ...shopPosts];
+
+  const feedItems = allPosts.map((post) => ({
     title: post.data.title,
     link: `/${post.slug}/`,
     pubDate: post.data.publishDate,
+    category: post.data.category, // Include 'category' field
     content: sanitizeHtml(parser.render(post.body)),
     ...post.frontmatter,
-    
   }));
 
   return rss({
